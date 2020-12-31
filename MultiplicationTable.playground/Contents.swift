b5e37bed639e21fd3eb64610aa498b44dacadbd7
Ray <ddarjae@naver.com>
2021-01-01 08:59:09 +0900

    Study variable and constant study concenpt of variable and constant as muliplication table

--- MultiplicationTable.playground/Contents.swift
+++ MultiplicationTable.playground/Contents.swift
@@ -1,7 +1,21 @@
 import UIKit
 
-for i in 2...3 {
-    for j in 1...9 {
-        print("\(i) X \(j) = \(i * j)")
-    }
-}
+print(2 * 1)
+print(2 * 2)
+print(2 * 3)
+print(2 * 4)
+print(2 * 5)
+print(2 * 6)
+print(2 * 7)
+print(2 * 8)
+print(2 * 9)
+
+print(3 * 1)
+print(3 * 2)
+print(3 * 3)
+print(3 * 4)
+print(3 * 5)
+print(3 * 6)
+print(3 * 7)
+print(3 * 8)
+print(3 * 9)
---
+++ MultiplicationTable45.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+let table4 = 4
+let table5 = 5
+
+var result = table4 * 1
+print(result)
+result = table4 * 2
+print(result)
+result = table4 * 3
+print(result)
+result = table4 * 4
+print(result)
+result = table4 * 5
+print(result)
+result = table4 * 6
+print(result)
+result = table4 * 7
+print(result)
+result = table4 * 8
+print(result)
+result = table4 * 9
+print(result)
+
+result = table5 * 1
+print(result)
+result = table5 * 2
+print(result)
+result = table5 * 3
+print(result)
+result = table5 * 4
+print(result)
+result = table5 * 5
+print(result)
+result = table5 * 6
+print(result)
+result = table5 * 7
+print(result)
+result = table5 * 8
+print(result)
+result = table5 * 9
+print(result)
